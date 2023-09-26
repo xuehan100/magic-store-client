d47ca314d5363223d46052d5a00eb8c6eeb0faac
Xuehan Cheng <xuehan@Xuehans-MacBook-Pro.local>
2023-09-26 15:43:52 -0700

    fetch data during SSR

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-export default () => {
+import axios from "axios";
+import { CURRENT_USER_API_PATH } from "../constants";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
+    // on the server, req should be made to http://ingress-nginx.ingress-nginx.svc.cluster.local
+    const response = await axios.get(
+      `http://ingress-nginx-controller.ingress-nginx.svc.cluster.local${CURRENT_USER_API_PATH}`,
+      { headers: req.headers }
+    );
+    return response.data;
+  } else {
+    // on the browser, req should be made to a base url of ''
+    const response = await axios.get(CURRENT_USER_API_PATH);
+    return response.data;
+  }
+  return {};
+};
+
+export default LandingPage;
